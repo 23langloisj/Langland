b2254db37d7e45b52583e5c847411b0d0c1668c1
23langloisj <23langloisj@gmail.com>
2024-07-10 22:38:16 -0400

    attempt to fix gravity --> still broken

--- Langland/Assets/PlayerMovement.cs
+++ Langland/Assets/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -13,9 +14,25 @@ public class PlayerMovement : MonoBehaviour
 
     Vector3 velocity;
 
+    public Transform groundCheck;
+
+    public float groundDistance = 0.4f;
+
+    public LayerMask groundMask;
+
+    bool isGrounded;
+
     // Update is called once per frame
     void Update()
     {
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y > 0)
+        {
+            velocity.y = -2f;
+        }
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
