555bc41d5abc1b698c542acc9a66e82ad6d1aeeb
23langloisj <23langloisj@gmail.com>
2024-07-12 13:22:37 -0400

    progress

--- Langland/Assets/TogglePOV.cs
+++ Langland/Assets/TogglePOV.cs
@@ -1,18 +1,34 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TogglePOV : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject mainCamera; // Assign this in the Inspector
+    public Vector3 offsetFromPlayer1 = new Vector3(0, 0.62f, 0); // Offset from player for POV 1
+    public Vector3 offsetFromPlayer2 = new Vector3(0, 3, -11);  // Offset from player for POV 2
+
+    public GameObject player; // Reference to the Player GameObject
+
     void Start()
     {
-        
+
+        if (mainCamera != null && player != null)
+        {
+            // Initialize the camera position relative to the player
+            mainCamera.transform.position = player.transform.position + offsetFromPlayer1;
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Check if F key is pressed
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            // Toggle between two POV positions
+            if (mainCamera != null && player != null)
+            {
+                mainCamera.transform.position = player.transform.position + offsetFromPlayer2;
+            }
+        }
     }
 }
